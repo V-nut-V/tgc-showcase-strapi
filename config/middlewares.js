c9317a1e420b163d98bacfd67d5967ddea143ecd
Harley Lin <harley9387@gmail.com>
2025-07-20 16:43:55 +1200

    add meida file uploader provider - 'aliyun', and image/video media upload input to the product collection

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,29 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "script-src": ["'self'", "'unsafe-inline'", "cdn.jsdelivr.net"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "cdn.jsdelivr.net",
+            "strapi.io",
+            `${env("BUCKET")}.${env("REGION")}.aliyuncs.com`,
+          ],
+        },
+      },
+    },
+  },
+  // 'strapi::security',
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
